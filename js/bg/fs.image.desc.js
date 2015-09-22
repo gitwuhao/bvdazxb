3212aa6652f88fe6d3f7a5aeccb53f3451f5a0c6
https://github.com/gitwuhao/asdfghjkl.git <https://github.com/gitwuhao/asdfghjkl.git>
2015-09-22 17:16:43 +0800

    add

--- js/bg/fs.image.desc.js
+++ js/bg/fs.image.desc.js
@@ -2,15 +2,16 @@
 
     classjs({
         className: 'fs.image.desc',
-        extendEvent: true,
+        extend: 'fs.job',
         shops: config.shops,
         shop: config.shops[0],
         type: 'pc',
         PC_TYPE: 'pc',
         H5_TYPE: 'h5',
         data_type: 'handu_pc',
+        task_type: 'desc',
         ready: function() {
-            this.loadItem();
+            this.callSuper();
             this.initEvent();
             fsPlugin.register(this);
         },
@@ -34,24 +35,27 @@
         doCaptureImage: function(event) {
             var item = this.activeItem;
             var dir = this.itemDirMap[item.id];
+            var shop_name = this.shop.name;
+            var file_name = item.key + '_' + shop_name + '_desc_' + event.index + '.' + event.format;
+            this.activeWin.document.title = file_name;
             this.server.request('uploadImage', {
                 dirId: dir.dir_id,
                 data: event.data,
                 rate: '0.95',
                 itemId: item.id,
                 index: event.index,
-                file_name: item.key + '_desc_' + event.index + '.' + event.format
+                file_name: file_name
             }, function() {
 
             });
         },
         doCaptureDone: function() {
             var me = this;
+            this.activeWin.document.title = 'doCaptureDone...';
             setTimeout(function() {
                 var item = me.activeItem;
-                me.itemData.index = me.itemIdMapIndex[item.id] + 1;
-                me.uploadDescJobData();
-                me.activeWin.location.reload();
+                me.uploadJob(item.id);
+                // me.activeWin.location.reload();
             }, 1000);
         },
         createTab: function() {
@@ -154,95 +158,6 @@
             var item = this.metaItemMap[request.itemId];
             item.urls = item.urls || {};
             item.urls[request.index] = request.url;
-        },
-        loadItem: function() {
-            var me = this;
-            $.ajax({
-                type: 'POST',
-                async: false,
-                url: config.urls.data + this.getDescJobFileName(),
-                dataType: 'text',
-                success: function(data) {
-                    me.doLoadItems(JSON.parse(data));
-                },
-                error: function() {
-                    me.initLoadItemData();
-                }
-            });
-        },
-        initLoadItemData: function() {
-            var me = this;
-            $.ajax({
-                type: 'POST',
-                async: false,
-                url: config.urls.data + this.shop.id + '_default.json',
-                dataType: 'text',
-                success: function(data) {
-                    data = JSON.parse(data);
-                    data.index = 0;
-                    me.doLoadItems(data);
-                },
-                error: function() {}
-            });
-        },
-        doLoadItems: function(data) {
-            this.itemData = data;
-            var map = {};
-            var metaItemMap = {};
-
-            util.each(data.list, function(i, item) {
-                map[item.id] = i;
-                metaItemMap[item.id] = item;
-            });
-            this.itemIdMapIndex = map;
-            this.metaItemMap = metaItemMap;
-            this.loadItemDirMap();
-        },
-        getItemDirMapFileName: function() {
-            return this.shop.id + '_dir_' + this.data_type + '.json';
-        },
-        loadItemDirMap: function() {
-            var me = this;
-            $.ajax({
-                type: 'POST',
-                async: false,
-                url: config.urls.data + this.getItemDirMapFileName(),
-                dataType: 'text',
-                success: function(data) {
-                    me.doLoadItemDirMap(JSON.parse(data));
-                },
-                error: function() {}
-            });
-        },
-        doLoadItemDirMap: function(data) {
-            this.itemDirMap = data || {};
-            this.initServer();
-        },
-        getItem: function() {
-            var data = this.itemData;
-            var shop = this.shop;
-            var item = data.list[data.index];
-            if (!item || data.index >= 200) {
-                return null;
-            }
-            return util.merger({}, item, {
-                type: shop.name
-            });
-        },
-        getDescJobFileName: function() {
-            return this.shop.id + '_desc_job_' + this.data_type + '.json';
-        },
-        uploadDescJobData: function() {
-            $.ajax({
-                type: 'POST',
-                url: config.urls.upload,
-                data: {
-                    filename: this.getDescJobFileName(),
-                    data: JSON.stringify(this.itemData)
-                },
-                success: function() {},
-                error: function() {}
-            });
         }
     });
     classjs({
